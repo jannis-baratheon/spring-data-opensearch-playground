761c6dc281781d08c277f71067761be8e5730623
Janusz Wiśniowski <janusz.wisniowski@shift4.com>
2023-10-30 19:15:27 +0100

    Playing with the search

--- src/main/java/test/opensearch/TestDocument.java
+++ src/main/java/test/opensearch/TestDocument.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Document(indexName = "test_document")
 @Setting(settingPath = "index_settings.json")
 public record TestDocument(
-        @MultiField(mainField = @Field(type = FieldType.Text), otherFields = {@InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "tokenizer_analyzer", searchAnalyzer = "standard")}) String tokenizedText,
-        @MultiField(mainField = @Field(type = FieldType.Text), otherFields = {@InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "filter_analyzer", searchAnalyzer = "standard")}) String filteredText,
+        @MultiField(mainField = @Field(type = FieldType.Text), otherFields = {@InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "tokenizer_analyzer")}) String tokenizedText,
+        @MultiField(mainField = @Field(type = FieldType.Text), otherFields = {@InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "filter_analyzer")}) String filteredText,
         @Field(type = FieldType.Object) List<SubObject> subObjects) {
 }
--- src/test/java/test/opensearch/OpenSearchTest.java
+++ src/test/java/test/opensearch/OpenSearchTest.java
@@ -4,7 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.AnalyzeRequest;
+import org.opensearch.client.indices.AnalyzeResponse;
 import org.opensearch.data.client.orhlc.NativeSearchQueryBuilder;
+import org.opensearch.index.query.MultiMatchQueryBuilder;
+import org.opensearch.index.query.Operator;
 import org.opensearch.testcontainers.OpensearchContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,11 +23,13 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.opensearch.index.query.QueryBuilders.matchQuery;
+import static org.opensearch.index.query.QueryBuilders.multiMatchQuery;
 
 @Slf4j
 @SpringBootTest
@@ -39,6 +47,8 @@ class OpenSearchTest {
                     .withEnv("action.auto_create_index", "false");
     @Autowired
     private ElasticsearchOperations elasticsearchOperations;
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
     private IndexOperations indexOperations;
 
     @DynamicPropertySource
@@ -71,6 +81,53 @@ class OpenSearchTest {
 
         // then
         assertThat(fetchAllDocuments()).singleElement().isEqualTo(expectedDocument);
+    }
+
+    @Test
+    void synonymsAnalyzerWorks() throws IOException {
+        // given
+        var analyzeRequest = AnalyzeRequest.withIndexAnalyzer(
+                "test_document",
+                "synonyms_analyzer",
+                "first Diners Club last");
+
+        // when
+        var analysisResult = restHighLevelClient.indices().analyze(analyzeRequest, RequestOptions.DEFAULT);
+
+        // then
+        assertThat(analysisResult.getTokens())
+                .extracting(AnalyzeResponse.AnalyzeToken::getTerm)
+                .contains("first", "diners_club", "DINERS_CLUB", "club", "Diners", "Club", "last");
+    }
+
+    @Test
+    void searchesDocumentsWithSynonyms2() {
+        // given
+        var expectedMatch = new TestDocument(
+                "DINERS_CLUB",
+                "some other text",
+                List.of());
+        var expectedNonMatch = new TestDocument(
+                "any text",
+                "any other text",
+                List.of());
+        indexDocuments(expectedMatch, expectedNonMatch);
+
+        // when
+        var actualSearchResult = elasticsearchOperations.search(
+                new NativeSearchQueryBuilder()
+                        .withQuery(multiMatchQuery("Diners Club some", "filteredText", "tokenizedText")
+                                .operator(Operator.AND)
+                                .type(MultiMatchQueryBuilder.Type.CROSS_FIELDS)
+                                .analyzer("synonyms_analyzer"))
+                        .build(),
+                TestDocument.class);
+
+        // then
+        assertThat(actualSearchResult.getSearchHits())
+                .extracting(SearchHit::getContent)
+                .singleElement()
+                .isEqualTo(expectedMatch);
     }
 
     @Test
